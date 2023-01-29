ffd6a7f2c3b62f6010a64a7b8ed0e4c349f8923d
InigoCG <inigocg@gmail.com>
2023-01-29 18:32:12 +0100

    Valid Parentheses

---
+++ LeetCode/ValidParentheses.java
@@ -0,0 +1,53 @@
+package LeetCode;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        // Create a new stack to store the characters.
+        Stack<Character> stack = new Stack<>();
+
+
+        // convert string into char array and access the characters using for each loop.
+        for(char ch: s.toCharArray())
+        {
+            // check ch
+            switch (ch) {
+                // open bracket then push it in stack.
+                // close bracket then pop the item and compare.
+                case '(', '{', '[' -> stack.push(ch);
+                case ')' -> {
+                    if (stack.isEmpty() || stack.pop() != '(')
+
+                    // if the stack is empty then it means string have no open bracket.
+                    // hence it is invalid.
+                    {
+                        return false;
+                    }
+                }
+                case '}' -> {
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                }
+                case ']' -> {
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                }
+            }
+        }
+
+
+        // After the loop we have to check one more condition.
+        // return true only if the stack is empty.
+        // if stack is not empty that means we have unused brackets.
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "(";
+        System.out.println(isValid(s));
+    }
+}
